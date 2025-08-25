9ca87c987562fdbb13a225074a677aab6a0ce767
SIMRAN11288 <bartwalsimran@gmail.com>
2025-08-25 23:17:29 +0530

    project.py

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
-from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
@@ -127,6 +127,7 @@ elif choice=="enter 1/2/3 to choose story continuation option":
 final_story=" ".join(st.session_state.story)
 st.write("Final Story")
 st.success(final_story)
+
 
 
 
